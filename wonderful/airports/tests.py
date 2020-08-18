403e9d68ea010317f9e56b471f2d477da3c1c0e0
superdev1994 <sanchzxel@gmail.com>
2020-08-18 00:24:49 -0300

    dummy test

--- wonderful/airports/tests.py
+++ wonderful/airports/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from airports.models import Airport
 
 # Create your tests here.
+
+
+class TestAirport(TestCase):
+    def setUp(self):
+        self.airport = Airport()
+        self.airport.save()
